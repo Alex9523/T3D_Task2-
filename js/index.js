2cc41d7718841d3ef8623b28ba137f93389412f3
Alex9523 <siaskyi9523@gmail.com>
2020-07-22 14:11:50 +0300

    add orbit control

--- js/index.js
+++ js/index.js
@@ -9,16 +9,20 @@ window.onload = function () {
     canvas.setAttribute('height', height)
 
 
-    const controls = new function(){
-        this.rotationSpeed = 0.02
+    const controllers = new function(){
+        //controle colors
         this.color = '#ff9966'
         this.backgroundColor = '#000000'
+
+        //control light
+        this.intensive = 1
     }
 
     const gui = new dat.GUI();
-    gui.add(controls, 'rotationSpeed', 0, 0.5)
-    const controllerColor = gui.addColor(controls, 'color')
-    const controllerBackgroundColor = gui.addColor(controls, 'backgroundColor')
+
+    const controllerColor = gui.addColor(controllers, 'color')
+    const controllerBackgroundColor = gui.addColor(controllers, 'backgroundColor')
+    const controllerIntensive = gui.add(controllers, 'intensive')
 
 
     controllerColor.onChange(function(val){ //change color of cube
@@ -28,6 +32,10 @@ window.onload = function () {
     controllerBackgroundColor.onChange(function(val){   //change color of background
         renderer.setClearColor(`${val}`) 
      })
+
+    // controllerIntensive.onChange(function(val){ //change color of cube
+    //     controls.intensive = val   
+    // })
 
     const renderer = new THREE.WebGLRenderer({ canvas: canvas })
 
@@ -39,20 +47,21 @@ window.onload = function () {
     // scene.add(light)
 
     const color = 0xFFFFFF;
-    const intensity = 1;
-    const light = new THREE.AmbientLight(color, intensity);
+    //const intensive = 1
+    const light = new THREE.AmbientLight(color, controllers.intensive);
     scene.add(light);
 
     const fov = 45;
     const aspect = width / height;
     const near = 0.1;
-    const far = 1000;
+    const far = 5000;
 
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
     camera.position.set(0, 0, 1000)
 
     const geometryCube = new THREE.BoxGeometry(200, 200, 200, 4, 4)
-    const materialCube = new THREE.MeshStandardMaterial()
+    const materialCube = new THREE.MeshStandardMaterial()//{wireframe: true} грани
+    
     materialCube.color.set(0xff9966);
 
 
@@ -61,14 +70,14 @@ window.onload = function () {
 
     scene.add(cube)
 
-
-    //renderer.render(scene, camera)
-
+    const control = new THREE.OrbitControls (camera, canvas);
+    control.update();
+    control.target.set(0, 5, 0);
+    
     function loop(){
-        cube.rotation.y += controls.rotationSpeed
-        cube.rotation.x += controls.rotationSpeed
-
+    
         renderer.render(scene, camera)
+       
         requestAnimationFrame(() => {loop()})
     }
 
